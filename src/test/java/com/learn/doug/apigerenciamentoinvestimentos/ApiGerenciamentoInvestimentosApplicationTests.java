bb6aa5f39030df140464b48b7ab4090e38106982
Thifon <douglas.f.brito@gmail.com>
2020-08-05 18:27:02 -0300

    Start commit

---
+++ src/test/java/com/learn/doug/apigerenciamentoinvestimentos/ApiGerenciamentoInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.doug.apigerenciamentoinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciamentoInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
